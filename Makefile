815da2b476b113cd23a96a93011e2d5e4a775044
Rudransh Kala <rkal0759@uni.sydney.edu.au>
2026-05-24 19:23:05 +1000

    feat: finished make file

--- Makefile
+++ Makefile
@@ -1,18 +1,31 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -g -I./header -Ilibanimate/include
+
+SERVER_SRCS = animate_server.c buffer.c client.c command.c message.c manager.c network.c struct.c
+SERVER_OBJS = $(SERVER_SRCS:.c=.o)
+
+CLIENT_SRCS = animate_client.c network.c
+CLIENT_OBJS = $(CLIENT_SRCS:.c=.o)
+
+.PHONY: default clean libanimate
 
 default: animate_server animate_client
 
 libanimate:
-	@if [ ! -d $@ ]; then \
-		echo "ERROR: libanimate not found" > /dev/stderr; \
-		echo "Download and unzip libanimate.zip from P2 resources" > /dev/stderr; \
+	@if [ ! -d libanimate ]; then \
+		echo "ERROR: libanimate not found" >&2; \
+		echo "Download and unzip libanimate.zip from P2 resources" >&2; \
 		false; \
 	fi
 
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
-animate_server: animate_server.c | libanimate
-	gcc $^ -I libanimate/include -Llibanimate/lib -lanimate -o $@
+animate_server: libanimate $(SERVER_OBJS)
+	$(CC) $(CFLAGS) $(SERVER_OBJS) -Llibanimate/lib -lanimate -o $@
 
-animate_client: animate_client.c
-	gcc $^ -o $@
+animate_client: $(CLIENT_OBJS)
+	$(CC) $(CFLAGS) $(CLIENT_OBJS) -o $@
 
-
+clean:
+	rm -f animate_server animate_client $(SERVER_OBJS) $(CLIENT_OBJS)
--- header/manager.h
+++ header/manager.h
@@ -6,12 +6,32 @@
 #include <stdbool.h>
 #include <pthread.h>
 
-struct dynamic_manager;
+/*************************************************************************
+    Dynamic Manager Structs
+-------------------------------------------------------------------------*/
+
+struct dynamic_manager
+{
+    int              number_items;
+    int              capacity_items;
+    void     **      struct_array;
+    
+    pthread_mutex_t  manager_mutex;
+};
+
+/*************************************************************************
+    Dynamic Manager Member Functions
+-------------------------------------------------------------------------*/
 
 int dynamic_manager_init (struct dynamic_manager ** dynamic_manager);
 int push_dynamic_manager (struct dynamic_manager * dynamic_manager, void * new_value);
-void destory_dynamic_manager(struct dynamic_manager * dynamic_manager);
 int pop_dynamic_manager_item(struct dynamic_manager * dynamic_manager, void ** returned_value, int index);
 int check_dynamic_manager (struct dynamic_manager * dynamic_manager, void * selected_item);
+int find_selected_item(struct dynamic_manager * dynamic_manager, void * selected_item, void ** returned_value);
+int pop_selected_item(struct dynamic_manager * dynamic_manager, void * selected_item, void ** returned_value);
+void get_item(struct dynamic_manager * dynamic_manager, int index, void ** returned_value);
+int get_number_items(struct dynamic_manager * dynamic_manager);
+int get_capacity_items(struct dynamic_manager * dynamic_manager);
+
 
 #endif
